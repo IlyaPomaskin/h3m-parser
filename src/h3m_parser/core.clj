bdbfe53a5571e6719c4b10b9c3ff090e0380ee2e
Ilya Pomaskin <ilya-pomaskin@yandex-team.ru>
2019-04-09 20:03:00 +0300

    add file check

--- src/h3m_parser/core.clj
+++ src/h3m_parser/core.clj
@@ -4,8 +4,7 @@
             [clojure.pprint :as pp]
             [h3m-parser.objects :as h3m-objects]
             [h3m-parser.h3m :as h3m]
-            [clojure.java.io :as io])
-  (:import org.clojars.smee.binary.core.BinaryIO))
+            [clojure.java.io :as io]))
 
 
 (defn stream->bytes [is]
@@ -27,23 +26,19 @@
 
 
 (defn -main [file-path]
-  (when (not (.exists (io/file file-path)))
-    (println "File" file-path "not exists"))
-  (some-> file-path
-          (as-> path (when (.exists (io/file path))
-                       path))
-          (io/input-stream)
-          (java.util.zip.GZIPInputStream. 1)
-          (io/input-stream)
-          (as-> stream (b/decode h3m/root stream))
-          (dissoc :players)
-          (dissoc :predefined-heroes)
-          (dissoc :placeholder-1)
-          (dissoc :placeholder-2)
-          (dissoc :placeholder-3)
-          (dissoc :terrain)
-          (dissoc :defs)
-          (pp/pprint)))
-
-
+  (if (not (.exists (io/file file-path)))
+    (println "File" file-path "not exists")
+    (some-> file-path
+            (io/input-stream)
+            (java.util.zip.GZIPInputStream. 1)
+            (io/input-stream)
+            (as-> stream (b/decode h3m/root stream))
+            (dissoc :players)
+            (dissoc :predefined-heroes)
+            (dissoc :placeholder-1)
+            (dissoc :placeholder-2)
+            (dissoc :placeholder-3)
+            (dissoc :terrain)
+            (dissoc :defs)
+            (pp/pprint))))
 
