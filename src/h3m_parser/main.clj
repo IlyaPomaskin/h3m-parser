6b43734b495d775fa68640ecee0b97941ab05d02
Ilya Pomaskin <ilya.pomaskin@gmail.com>
2019-09-18 22:07:29 +0300

    remove cli

--- src/h3m_parser/main.clj
+++ src/h3m_parser/main.clj
@@ -2,10 +2,8 @@
   (:require [clojure.spec.alpha :as s]
             [clojure.java.io :as io]
             [org.clojars.smee.binary.core :as binary]
-            [cheshire.core :as cheshire]
             [h3m-parser.objects :as h3m-objects]
-            [h3m-parser.h3m :as h3m]
-            ))
+            [h3m-parser.h3m :as h3m]))
 
 
 (defn parse-stream [stream]
@@ -13,17 +11,8 @@
 
 
 (defn parse-file [file-path]
-  (if (not (.exists (io/file file-path)))
-    (throw (Exception. (str "File " file-path " doesn't exists")))
-    (some-> file-path
-            (io/input-stream)
-            (java.util.zip.GZIPInputStream. 1)
-            (io/input-stream)
-            (parse-stream))))
-
-
-(defn -main [file-path]
-    (-> file-path
-        (parse-file)
-        (cheshire/generate-string {:pretty true})
-        (println)))
+  (some-> file-path
+          (io/input-stream)
+          (java.util.zip.GZIPInputStream. 1)
+          (io/input-stream)
+          (parse-stream)))
