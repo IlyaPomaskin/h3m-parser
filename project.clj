1d30f1a07716ca5991b41f8917d81b4e0000cecd
Ilya Pomaskin <ilya.pomaskin@gmail.com>
2019-09-18 22:07:14 +0300

    remove unused deps

--- project.clj
+++ project.clj
@@ -4,9 +4,7 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.0"]
-                 [smee/binary "0.5.4"]
-                 [gloss "0.2.6"]
-                 [cheshire "5.8.1"]]
+                 [smee/binary "0.5.4"]]
   :main ^:skip-aot h3m-parser.main
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
