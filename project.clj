33513e872e13420c4cd6fb504e2fe043ad5954cf
Ilya Pomaskin <ilya.pomaskin@gmail.com>
2019-09-18 22:21:49 +0300

    remove building

--- project.clj
+++ project.clj
@@ -4,7 +4,4 @@
   :license {:name "GPL-3.0"
             :url "https://www.gnu.org/licenses/gpl-3.0.en.html"}
   :dependencies [[org.clojure/clojure "1.10.0"]
-                 [smee/binary "0.5.4"]]
-  :main ^:skip-aot h3m-parser.core
-  :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+                 [smee/binary "0.5.4"]])
