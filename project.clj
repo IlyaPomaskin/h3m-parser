af3db04e86ba9f86c3f29a2359f92099d39a4fc9
Ilya Pomaskin <ilya.pomaskin@gmail.com>
2019-09-18 22:19:16 +0300

    change license

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject h3m-parser "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
+(defproject h3m-parser "1.0.0"
+  :description "Library for parsing *.h3m (Heroes of Might and Magic III maps)"
+  :url "https://github.com/IlyaPomaskin/h3m-parser/"
+  :license {:name "GPL-3.0"
+            :url "https://www.gnu.org/licenses/gpl-3.0.en.html"}
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [smee/binary "0.5.4"]]
   :main ^:skip-aot h3m-parser.main
