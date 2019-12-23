766ab04289eeab9c64f89f9d6455391857305ef5
Ilya Pomaskin <ilya.pomaskin@gmail.com>
2019-12-23 19:29:55 +0300

    add parse-def fn

--- src/h3m_parser/core.clj
+++ src/h3m_parser/core.clj
@@ -2,7 +2,8 @@
   (:require [clojure.java.io :as io]
             [org.clojars.smee.binary.core :as binary]
             [h3m-parser.h3m :as h3m]
-            [h3m-parser.lod :as lod]))
+            [h3m-parser.lod :as lod]
+            [h3m-parser.def :as def]))
 
 
 (defn parse-h3m [file-path]
@@ -17,3 +18,9 @@
   (some-> file-path
           (io/input-stream)
           (as-> stream (binary/decode lod/root stream))))
+
+
+(defn parse-def [file-path]
+  (some-> file-path
+          (io/input-stream)
+          (as-> stream (binary/decode def/root stream))))
