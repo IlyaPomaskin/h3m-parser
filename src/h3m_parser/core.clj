ba11d54c78c820e1169c3bbbc94e1018e97db5cf
Ilya Pomaskin <ilya.pomaskin@gmail.com>
2019-12-23 19:33:46 +0300

    add parse-def-frame fn

--- src/h3m_parser/core.clj
+++ src/h3m_parser/core.clj
@@ -3,7 +3,8 @@
             [org.clojars.smee.binary.core :as binary]
             [h3m-parser.h3m :as h3m]
             [h3m-parser.lod :as lod]
-            [h3m-parser.def :as def]))
+            [h3m-parser.def :as def]
+            [h3m-parser.frame :as frame]))
 
 
 (defn parse-h3m [file-path]
@@ -24,3 +25,13 @@
   (some-> file-path
           (io/input-stream)
           (as-> stream (binary/decode def/root stream))))
+
+
+(defn parse-def-frame [file-path group-index file-index]
+  (let [def-info (parse-def file-path)
+        name (get-in def-info [:groups group-index :names file-index])
+        offset (get-in def-info [:groups group-index :offsets file-index])
+        ; TODO legacy detection
+        legacy-def? false]
+    (when (and (some? name) (some? offset))
+      (frame/parse file-path offset legacy-def?))))
