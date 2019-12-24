61ffc89b37e433052b96d7c5b9daa524259d9928
Ilya Pomaskin <ilya.pomaskin@gmail.com>
2019-12-24 10:17:33 +0300

    disable sign

--- project.clj
+++ project.clj
@@ -4,5 +4,6 @@
   :global-vars {clojure.core/*warn-on-reflection* true}
   :license {:name "GPL-3.0"
             :url "https://www.gnu.org/licenses/gpl-3.0.en.html"}
+  :deploy-repositories [["clojars" {:sign-releases false}]]
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [smee/binary "0.5.4"]])
