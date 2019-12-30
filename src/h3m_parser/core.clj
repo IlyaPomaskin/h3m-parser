9920b6f2f88b3bdeee85ff5985e559e78836c456
Ilya Pomaskin <ilya.pomaskin@gmail.com>
2019-12-30 11:37:01 +0300

    change api

--- src/h3m_parser/core.clj
+++ src/h3m_parser/core.clj
@@ -10,27 +10,19 @@
    [java.util.zip Inflater InflaterInputStream GZIPInputStream]))
 
 
-(defn parse-h3m [file-path]
-  (some-> file-path
-          (io/input-stream)
-          (GZIPInputStream.)
-          (io/input-stream)
-          (as-> stream (binary/decode h3m/root stream))))
+(defn parse-h3m [in]
+  (binary/decode h3m/root (new GZIPInputStream in)))
 
 
-(defn parse-lod [file-path]
-  (some-> file-path
-          (io/input-stream)
-          (as-> stream (binary/decode lod/root stream))))
+(defn parse-lod [in]
+  (binary/decode lod/root in))
 
 
-(defn parse-def [file-path]
-  (some-> file-path
-          (io/input-stream)
-          (as-> stream (binary/decode def/root stream))))
+(defn parse-def [in]
+  (binary/decode def/root in))
 
 
-(defn def->stream
+(defn get-def-stream-from-lod
   [lod-def-info ^FileInputStream in]
   (let [{compressed-size :compressed-size
          offset :offset} lod-def-info]
@@ -40,65 +32,74 @@
       in)))
 
 
-(defn read-lod-def
+(defn parse-def-from-lod
   [lod-def-info in]
-  (let [def-stream (def->stream lod-def-info in)
-        def-info (binary/decode def/root def-stream)
-        uncompressed-size (:size lod-def-info)]
-    (-> def-info
-        (assoc
-         :name (:name lod-def-info)
-         :legacy? (def/legacy? def-info def-stream uncompressed-size)
-         :frames-count (get-in def-info [:groups 0 :frame-count]))
-        (dissoc :frames :palette))))
+  (let [{uncompressed-size :size
+         name :name} lod-def-info
+        def-stream (get-def-stream-from-lod lod-def-info in)
+        def-info (parse-def def-stream)
+        legacy? false ; (def/legacy? def-info def-stream uncompressed-size)
+        ]
+    (assoc
+     def-info
+     :legacy? legacy?
+     :name name)))
 
 
 (def map-objects
-  #{(:sprite def/def-type)
-    (:terrain def/def-type)
-    (:map def/def-type)
-    (:map-hero def/def-type)})
+  #{(:terrain def/def-type)
+    (:map def/def-type)})
 
 
-(defn read-frames [in]
-  (->> (binary/decode lod/root in)
-       :files
-       (filter #(map-objects (:type %)))
-       (filter #(clojure.string/ends-with? (:name %) ".def"))
-       (map #(read-lod-def % in))
-       (map #(do
-               (println (:name %) (:type %))
-               nil))
-       (count)))
+(defn lines->bytes [frame]
+  (let [{lines :lines
+         offsets :offsets} frame]
+    (mapcat
+      (fn [offset] (mapcat :data (get lines offset)))
+      offsets)))
 
 
-(clojure.pprint/pprint
- (read-frames
-  (new FileInputStream "./resources/H3sprite.lod")))
+(defn map-frame [frame]
+  (-> frame
+      (select-keys [:full-width
+                    :full-height
+                    :width
+                    :height
+                    :x
+                    :y
+                    :data])
+      (update :data lines->bytes)))
 
 
-(defn test-reading-def []
-  (clojure.pprint/pprint
-   (let [def-info (binary/decode def/root (io/input-stream "./resources/AvWAngl.def"))
-         palette (:palette def-info)
-         frame-0 (get-in def-info [:groups 0 :frames 1310])
-         frame-0-pixels (mapcat
-                         (fn [offset]
-                           (mapcat
-                            :data
-                            (get-in frame-0 [:data :lines offset])))
-                         (get-in frame-0 [:data :offsets]))]
-     {:palette palette
-      :full-width (:full-width frame-0)
-      :full-height (:full-height frame-0)
-      :width (:width frame-0)
-      :height (:height frame-0)
-      :x (:x frame-0)
-      :y (:y frame-0)
-      :pixels frame-0-pixels})
-  ;  (clojure.java.io/writer "AvWAngl-frame-0.edn")
-   ))
+(defn map-def [def-info]
+  (-> def-info
+      (select-keys [:name
+                    :full-width
+                    :full-height
+                    :palette
+                    :frames])
+      (update :frames #(map map-frame %))
+      (assoc :order (get-in def-info [:groups 0 :offsets]))))
 
 
-(comment
-  (test-reading-def))
+(defn lod->texture-atlas [^FileInputStream lod-in]
+  (let [lod-info (binary/decode lod/root lod-in)]
+    (->> (:files lod-info)
+         (filter #(map-objects (:type %)))
+         (filter #(clojure.string/ends-with? (:name %) "AvWAngl.def"))
+         (filter #(clojure.string/ends-with? (:name %) ".def"))
+         (map #(parse-def-from-lod % lod-in))
+         (filter #(false? (:legacy? %)))
+         (map map-def)
+         (take 1)
+        ;  (map #(do
+        ;          (println (:name %) (:group-count %) (:type %))
+        ;          nil))
+        ;  (count)
+         )))
+
+
+(lod->texture-atlas
+  (new
+   FileInputStream
+   "./resources/H3sprite.lod"))
