1e23161e2bd3d0194c594332bc0b38f8eeab36b8
Ilya Pomaskin <ilya.pomaskin@gmail.com>
2020-03-31 11:35:52 +0300

    update smee/binary

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject h3m-parser "1.2.3"
+(defproject h3m-parser "1.2.4"
   :description "Library for parsing *.h3m (Heroes of Might and Magic III maps)"
   :url "https://github.com/IlyaPomaskin/h3m-parser/"
   :global-vars {clojure.core/*warn-on-reflection* true}
@@ -6,4 +6,4 @@
             :url "https://www.gnu.org/licenses/gpl-3.0.en.html"}
   :deploy-repositories [["clojars" {:sign-releases false}]]
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [smee/binary "0.5.4"]])
+                 [smee/binary "0.5.5"]])
