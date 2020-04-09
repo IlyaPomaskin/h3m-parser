f96dcf12fc3f782d919d1163ebd23aab60faa2c3
Ilya Pomaskin <ilya.pomaskin@gmail.com>
2020-04-10 00:18:25 +0300

    fix deps

--- project.clj
+++ project.clj
@@ -4,5 +4,6 @@
   :license {:name "GPL-3.0"
             :url "https://www.gnu.org/licenses/gpl-3.0.en.html"}
   :deploy-repositories [["clojars" {:sign-releases false}]]
-  :dependencies [[org.clojure/clojure "1.9.0"]
-                 [smee/binary "0.5.5"]])
+  :exclusions [org.clojure/clojure]
+  :dependencies [[smee/binary "0.5.5"]]
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.10.0"]]}})
